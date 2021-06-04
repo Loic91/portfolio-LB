0ef1c6ca1af26f8c613cac7ff05e5cf3a8c16990
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-06-04 16:49:25 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 // OVERLAY
-// TweenMax.to(".overlay", 3.5, {
-//     delay: 3.9,
-//     // top: "-100%",
-//     right: "-100%",
-//     ease: Expo.easeInOut
-// });
+TweenMax.to(".overlay", 3.5, {
+    delay: 3.9,
+    // top: "-100%",
+    right: "-100%",
+    ease: Expo.easeInOut
+});
 
 
 // gsap.to('.box-color', 2.5, {
