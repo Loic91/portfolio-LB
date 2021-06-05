4b864fd1fdd3feec0326ae02d71e95973ab7fc89
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-06-06 00:11:13 +0200

    Add files via upload

--- color-scroll.js
+++ color-scroll.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() >= 0&& $(this).scrollTop() <= 400) {
@@ -18,24 +17,3 @@ $(function () {
        }
    });
 });
-=======
-$(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() >= 0&& $(this).scrollTop() <= 400) {
-           $(".scroll").css("background", "#c0392b").css("transition", "0.5s");
-        }
-       else if ($(this).scrollTop() >= 500 && $(this).scrollTop() <= 900) {
-          $(".scroll").css("background", "#e67e22").css("transition", "1.5s");
-       }
-       else if ($(this).scrollTop() >= 1000 && $(this).scrollTop() <= 1400) {
-          $(".scroll").css("background", "#8e44ad").css("transition", "1.5s");
-       }
-       else if ($(this).scrollTop() >= 1400 && $(this).scrollTop() <= 1900) {
-          $(".scroll").css("background", "#3498db").css("transition", "1.5s");
-       }
-       else if ($(this).scrollTop() >= 2000 && $(this).scrollTop() <= 2500) {
-          $(".scroll").css("background", "#2ecc71").css("transition", "0.5s");
-       }
-   });
-});
->>>>>>> 150eb903cee3eb356e6a82113e5c030bac8350a4
