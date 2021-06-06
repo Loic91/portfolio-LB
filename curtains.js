9a04b0cdd44ffe53350f1e6e354cd24be6c4fd59
Loic91 <62215559+Loic91@users.noreply.github.com>
2021-06-06 16:24:28 +0200

    Add files via upload

--- curtains.js
+++ curtains.js
@@ -133,7 +133,7 @@ window.addEventListener('load', () => {
                                 name: "uScrollStrength",
                                 type: "1f",
                                 // value: 2.5,
-                                value: 1.5,
+                                value: 2.5,
                             },
                         }
                     });
